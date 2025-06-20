8cb2f2865de1c80b17b8900d66515c1e82cd0bba
Chab Phanith <chabphanith@gmail.com>
2025-06-21 01:47:39 +0700

    first commit

---
+++ resources/views/vendor/users.blade.php
@@ -0,0 +1,130 @@
+@extends('vendor.includes.main')
+@push('title')
+<title>Users</title>
+@endpush
+
+@section('content')
+        
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                    <div class="card p-4 mt-4">
+                            <div class="row">
+                                <div class="col-xl-12 col-md-12">
+                                    <div class="d-flex">
+                                        <h4>Users</h4>
+                                        
+                                    </div>
+                                    <div class="mt-3">
+                                        <table id="datatablesSimple">
+                                            <thead>
+                                                <tr>
+                                                
+                                                <th scope="col">Sr. No.</th>
+                                                <th scope="col">Customer Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Address</th>
+                                                <th scope="col">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                               
+                                                <td>1</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+
+                                                <tr>
+                                               
+                                                <td>2</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+
+                                                <tr>
+                                               
+                                                <td>3</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+
+                                                <tr>
+                                                <td>4</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>5</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>6</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>7</td>
+                                                <td>Chab Phanith</td>
+                                                <td>Chabphanith@gmail</td>
+                                                <td>Reference site about Lorem Ipsum, giving information on its</td>
+                                                
+                                                <td>
+                                                    
+                                                    <a href="{{('order-detail')}}" class="btn btn-warning btn-sm"><i class="fa-regular fa-eye"></i></a>
+                                                </td>
+                                                </tr>
+
+                                                 
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                        
+                                </div>
+                            </div>
+                        </div>      
+                    </div>
+                </main>
+
+
+                
+
+@endsection
+                
